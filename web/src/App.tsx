bf8c221ccc6a6f9434ed9bf284f273d2f6d302bd
Sebastian Waisbrot <seppo0010@gmail.com>
2022-07-16 19:52:41 -0300

    add material

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Container from '@mui/material/Container';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container maxWidth="sm">
+      Bienvenide. Veamos qué materias nos interesan y horarios disponibles hay
+      para elegir una mejor inscripción.
+    </Container>
   );
 }
 
