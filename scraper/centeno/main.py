93c3e93737218966919f018d14608461deac0b15
Sebastian Waisbrot <seppo0010@gmail.com>
2022-07-16 14:42:52 -0300

    Parse centeno recommendations

---
+++ scraper/centeno/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+for f in ('cpc', 'cpo'):
+    xls = pd.ExcelFile(f'/data/recomendaciones_{f}.xlsx')
+    pages = []
+    for page_name in xls.sheet_names:
+        page = pd.read_excel(xls, page_name)
+        page.columns = page.iloc[1]
+        page = page.rename(columns={'COMISION': 'COMISIÓN', 'OPINION': 'OPINIÓN'})
+        page = page[~page['COMISIÓN'].isna() & ~page['OPINIÓN'].isna()][['COMISIÓN', 'OPINIÓN']]
+        page = page[page['OPINIÓN'] != 'SIN INFO.']
+        page = page[page['OPINIÓN'] != 'SIN INFO']
+        page = page[page['OPINIÓN'] != 'OPINIÓN']
+        page = page[page['OPINIÓN'] != 'OPINION']
+        pages.append(page)
+    df = pd.concat(pages)
+    df.to_json(f'recomendaciones_{f}.json', orient='records')
