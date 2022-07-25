12159190806db0769c8cd7020da443a743466bdb
Sebastian Waisbrot <seppo0010@gmail.com>
2022-07-25 13:13:58 -0300

    tucatedraderecho cpo scraper

--- scraper/tucatedraderecho/myspider.py
+++ scraper/tucatedraderecho/myspider.py
@@ -10,8 +10,24 @@ class MLStripper(HTMLParser):
         self.strict = False
         self.convert_charrefs= True
         self.text = StringIO()
+        self.tags = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'img': return
+        if tag == 'br': return
+        if tag == 'input': return
+        self.tags.append(tag)
+
+    def handle_endtag(self, tag):
+        if tag != self.tags[-1]:
+            raise Exception(f'Closing tag {tag}, stack is {">".join(self.tags)}')
+        self.tags = self.tags[:-1]
+
     def handle_data(self, d):
+        if 'script' in self.tags or 'style' in self.tags:
+            return
         self.text.write(d)
+
     def get_data(self):
         return self.text.getvalue()
 
@@ -29,37 +45,40 @@ class TuCatedraDerechoSpider(scrapy.Spider):
 
     def parse_real_home(self, response):
         for option in response.css('.CPC .combo option'):
-            materia_id = int(option.attrib['value'])
-            if materia_id > -1:
-                materia = option.css('::text').get()
-                yield response.follow('https://tucatedraderecho.com.ar/sumoNodoCombo.php?base=CPC&anexo=.CPC.info&Id=' + str(materia_id), self.parse_materia_cpc, meta={
-                    'materia': materia,
-                    'materia_id': materia_id,
+            option_id = int(option.attrib['value'])
+            if option_id > -1:
+                yield response.follow('https://tucatedraderecho.com.ar/sumoNodoCombo.php?base=CPC&anexo=.CPC.info&Id=' + str(option_id), self.parse_results, meta={'base': 'CPC', 'ref_id': option_id})
+
+        for option in response.css('.CPO .combo option'):
+            option_id = int(option.attrib['value'])
+            if option_id > -1:
+                yield response.follow('https://tucatedraderecho.com.ar/sumoNodoCombo.php?base=CPO&anexo=.CPO.info&Id=' + str(option_id), self.parse_results, meta={'base': 'CPO'})
+
+    def parse_results(self, response):
+        base = response.meta['base']
+        for option in response.css(':not(.materias) option'):
+            id = int(option.attrib['value'])
+            if id > -1:
+                yield response.follow(f'https://tucatedraderecho.com.ar/sumoNodoCombo.php?base={base}&anexo=.{base}.info&Id={id}', self.parse_results, meta={
+                    'base': base,
+                    'ref_id': id,
                 })
 
-    def parse_materia_cpc(self, response):
-        materia = response.meta['materia']
-        materia_id = response.meta['materia_id']
         for option in response.css('.materias option'):
-            docente_id = int(option.attrib['value'])
-            docente = option.css('::text').get()
-            if docente_id > -1:
-                yield response.follow(f'https://tucatedraderecho.com.ar/muestroInfo.php?base=CPC&anexo=.CPC.info&Id={docente_id}&id_materia={materia_id}', self.parse_materia_docente_cpc, meta={
-                    'materia': materia,
-                    'materia_id': materia_id,
-                    'docente': docente,
-                    'docente_id': docente_id,
+            ref_id = response.meta['ref_id']
+            id = int(option.attrib['value'])
+            if id > -1:
+                yield response.follow(f'https://tucatedraderecho.com.ar/muestroInfo.php?base={base}&anexo=.{base}.info&Id={id}&id_materia={ref_id}', self.parse_materia, meta={
+                    'base': base,
                 })
 
-    def parse_materia_docente_cpc(self, response):
-        for el in response.xpath('//ul/li//div[@id="tooltipinfo"]').extract():
+    def parse_materia(self, response):
+        for el in response.xpath('//ul/li').extract():
             text = strip_tags(el)
+            text = text.replace('INFORMACION RELEVADA', '')
+            text = text.replace('(click para copiar info)', '')
             text = re.sub(r'\n+', '\n', text)
             text = re.sub(r'^\s+', '', text, flags=re.MULTILINE)
             yield {
-                'materia': response.meta['materia'],
-                'materia_id': response.meta['materia_id'],
-                'docente': response.meta['docente'],
-                'docente_id': response.meta['docente_id'],
                 'text': text,
             }
