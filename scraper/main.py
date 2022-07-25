24d16b520b16a6b9797858a6e5141f00b70928c6
Sebastian Waisbrot <seppo0010@gmail.com>
2022-07-25 14:16:19 -0300

    Block of merger

--- scraper/main.py
+++ scraper/main.py
@@ -34,52 +34,60 @@ def is_docente_named(docente, tokens):
 oferta = read_json('./oferta/data/oferta_cpc.json')
 oferta.extend(read_json('./oferta/data/oferta_cpo.json'))
 
-cv_ = read_json('catedrasvirtuales_/data/parsed.json')
-for comment in cv_:
-    comment['tokens'] = set(re.split('[^a-zA-Z]', comment['text'].lower()))
-
-for o in tqdm(oferta):
+for o in oferta:
     o['catedrasvirtuales_'] = []
-    docente = re.sub(r'\s+[A-Z]\.$', '', o['docente'].split('-')[-1]).lower()
-    for comment in tqdm(cv_):
-        if (
-                is_same_materia(comment['materia_turno'], o['materia']) or
-                is_same_departamento(comment['materia_turno'], o.get('departamento', None))
-            ) and is_docente_named(docente, comment['tokens']):
-            o['catedrasvirtuales_'].append({
-                'shortcode': comment['shortcode'],
-                'text': comment['text'],
-                'sentiment': analyzer.predict(comment['text']).probas,
-            })
+    o['centeno'] = []
+    o['franja'] = []
 
 oferta_by_comision = {}
 for o in oferta:
     if isinstance(o, dict) and o['comision'] not in ('Comisión', 'comisión'):
         oferta_by_comision[int(o['comision'])] = o
 
-for o in oferta:
-    o['centeno'] = []
-    o['franja'] = []
 
-for f in 'cpc', 'cpo':
-    for op in tqdm(read_json(f'centeno/data/recomendaciones_{f}.json')):
-        for comision in str(op['COMISIÓN']).split('/'):
-            id_ = int(comision.strip())
-            if id_ not in oferta_by_comision:
-                continue
-            oferta_by_comision[id_]['centeno'].append({
-                'text': op['OPINIÓN'],
-                'sentiment': analyzer.predict(op['OPINIÓN']).probas,
-            })
+def process_cv():
+    cv_ = read_json('catedrasvirtuales_/data/parsed.json')
+    for comment in cv_:
+        comment['tokens'] = set(re.split('[^a-zA-Z]', comment['text'].lower()))
 
-for row in tqdm(read_json('franja/data/recommendations.json')):
-    id_ = int(row['comision'])
-    if id_ not in oferta_by_comision:
-        continue
-    oferta_by_comision[id_]['franja'].append({
-        'text': row['opinion'],
-        'sentiment': analyzer.predict(row['opinion']).probas,
-    })
+    for o in tqdm(oferta):
+        docente = re.sub(r'\s+[A-Z]\.$', '', o['docente'].split('-')[-1]).lower()
+        for comment in tqdm(cv_):
+            if (
+                    is_same_materia(comment['materia_turno'], o['materia']) or
+                    is_same_departamento(comment['materia_turno'], o.get('departamento', None))
+                ) and is_docente_named(docente, comment['tokens']):
+                o['catedrasvirtuales_'].append({
+                    'shortcode': comment['shortcode'],
+                    'text': comment['text'],
+                    'sentiment': analyzer.predict(comment['text']).probas,
+                })
+
+def process_centeno():
+    for f in 'cpc', 'cpo':
+        for op in tqdm(read_json(f'centeno/data/recomendaciones_{f}.json')):
+            for comision in str(op['COMISIÓN']).split('/'):
+                id_ = int(comision.strip())
+                if id_ not in oferta_by_comision:
+                    continue
+                oferta_by_comision[id_]['centeno'].append({
+                    'text': op['OPINIÓN'],
+                    'sentiment': analyzer.predict(op['OPINIÓN']).probas,
+                })
+
+def process_franja():
+    for row in tqdm(read_json('franja/data/recommendations.json')):
+        id_ = int(row['comision'])
+        if id_ not in oferta_by_comision:
+            continue
+        oferta_by_comision[id_]['franja'].append({
+            'text': row['opinion'],
+            'sentiment': analyzer.predict(row['opinion']).probas,
+        })
+
+process_cv()
+process_centeno()
+process_franja()
 
 with open('data/data.json', 'w') as fp:
     fp.write(json.dumps({
