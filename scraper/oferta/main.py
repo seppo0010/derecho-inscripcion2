15c34ada000ced89945d83267c52bee735bba1b1
Sebastian Waisbrot <seppo0010@gmail.com>
2023-02-06 10:08:11 -0300

    La oferta de CPO no vino con depto :shrug:

--- scraper/oferta/main.py
+++ scraper/oferta/main.py
@@ -11,9 +11,10 @@ df.to_json('oferta_cpc.json', orient='records')
 def clean_df(df):
     if df[1].isna().sum() == df[1].shape[0]:
         df = df.drop(1, axis=1)
+        df = df.rename(columns={v: i for i, v in enumerate(df.columns)})
     return df
 dfs = tabula.read_pdf("/data/oferta_cpo.pdf", pages='all', pandas_options={'header': None}, options='-l')
 df = pd.concat([clean_df(df) for df in dfs]).reset_index(drop=True)
 df = df.iloc[1: , 1:7]
-df.columns = ['materia','comision','modalidad','docente','horario','departamento']
+df.columns = ['materia','comision','modalidad','docente','horario']
 df.to_json('oferta_cpo.json', orient='records')
